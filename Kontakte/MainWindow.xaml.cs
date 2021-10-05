11aac5cdf310255e662308b9de9ee291c1f4cb5e
Nico Pape <87124169+nicopape@users.noreply.github.com>
2021-10-05 13:15:11 +0200

    Add files via upload

---
+++ Kontakte/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kontakte
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string p = " ";
+            string message = "Hallo " + txtBox_vorname.Text + p + txtBox_nachname.Text + p + "aus " + txtBox_ort.Text + p + "wir können dir eine email an " + txtBox_email.Text + p + "senden!";
+            lbl_message.Content = message;
+        }
+    }
+}
